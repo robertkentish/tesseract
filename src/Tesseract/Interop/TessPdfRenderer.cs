291ef787d17994f33ce5d67a8053c5693f430e72
Robert Kentish <robert.kentish@viatek.com.au>
2016-07-07 12:17:44 +1000

    Added PDF rendering functions to .NET wrapper

---
+++ src/Tesseract/Interop/TessPdfRenderer.cs
@@ -0,0 +1,67 @@
+﻿using InteropDotNet;
+using System;
+using System.Runtime.InteropServices;
+
+namespace Tesseract.Interop
+{
+    public interface ITessPdfRendererSignatures
+    {
+        /// <summary>
+        /// Creates a new TessPdfRenderer instance
+        /// </summary>
+        /// <returns></returns>
+        [RuntimeDllImport(Constants.TesseractDllName, CallingConvention = CallingConvention.Cdecl, EntryPoint = "TessPDFRendererCreate")]
+        IntPtr TessPdfRendererCreate(IntPtr outputBasePtr, IntPtr dataDirPtr);
+
+        /// <summary>
+        /// Deletes a TessPdfRenderer instance.
+        /// </summary>
+        /// <returns></returns>
+        [RuntimeDllImport(Constants.TesseractDllName, CallingConvention = CallingConvention.Cdecl, EntryPoint = "TessDeletePDFRenderer")]
+        void TessDeletePdfRenderer(HandleRef ptr);
+
+        /// <summary>
+        /// Begins the process of writing out a PDF document. Writes PDF header to file
+        /// </summary>
+        /// <param name="handle">Handle of the native PDF renderer instance</param>
+        /// <param name="titlePtr">Title for the PDF document</param>
+        /// <returns></returns>
+        [RuntimeDllImport(Constants.TesseractDllName, CallingConvention = CallingConvention.Cdecl, EntryPoint = "TessPDFRendererBeginDocument")]
+        int TessPDFRendererBeginDocumentHandler(HandleRef handle, IntPtr titlePtr);
+
+        /// <summary>
+        /// Closes out the PDF file after writing footer information
+        /// </summary>
+        /// <param name="handle">Handle of the native PDF renderer instance</param>
+        /// <returns></returns>
+        [RuntimeDllImport(Constants.TesseractDllName, CallingConvention = CallingConvention.Cdecl, EntryPoint = "TessPDFRendererEndDocument")]
+        int TessPDFRendererEndDocumentHandler(HandleRef handle);
+
+    }
+
+    class TessPdfRenderer
+    {
+        private static ITessPdfRendererSignatures native;
+
+        public static ITessPdfRendererSignatures Native
+        {
+            get
+            {
+                if (native == null)
+                    Initialize();
+                return native;
+            }
+        }
+
+        public static void Initialize()
+        {
+            if (native == null)
+            {
+                LeptonicaApi.Initialize();
+                native = InteropRuntimeImplementer.CreateInstance<ITessPdfRendererSignatures>();
+            }
+        }
+
+    }
+}
+
---
+++ src/Tesseract/PdfRenderer.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Text;
+using Tesseract.Interop;
+
+namespace Tesseract
+{
+    public class PdfRenderer : DisposableBase
+    {
+        private static readonly TraceSource trace = new TraceSource("PDFRenderer");
+
+        private HandleRef handle;
+
+        public PdfRenderer(string outputbase, string datadir)
+        {
+            DefaultPageSegMode = PageSegMode.Auto;
+
+            IntPtr dataDirPtr = IntPtr.Zero;
+            IntPtr outputBasePtr = IntPtr.Zero;
+
+            dataDirPtr = MarshalHelper.StringToPtr(datadir, Encoding.UTF8);
+            outputBasePtr = MarshalHelper.StringToPtr(outputbase, Encoding.UTF8);
+
+            handle = new HandleRef(this, Interop.TessPdfRenderer.Native.TessPdfRendererCreate(outputBasePtr, dataDirPtr));
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (handle.Handle != IntPtr.Zero)
+            {
+                Interop.TessPdfRenderer.Native.TessDeletePdfRenderer(handle);
+                handle = new HandleRef(this, IntPtr.Zero);
+            }
+        }
+
+        /// <summary>
+        /// Begins the process of writing out a PDF document
+        /// </summary>
+        /// <param name="title">The title for the PDF</param>
+        /// <returns></returns>
+        public bool BeginDocument(string title)
+        {
+            IntPtr titlePtr = IntPtr.Zero;
+            titlePtr = MarshalHelper.StringToPtr(title, Encoding.UTF8);
+
+            int value = Interop.TessPdfRenderer.Native.TessPDFRendererBeginDocumentHandler(handle, titlePtr);
+            bool ret = Convert.ToBoolean(value);
+
+            return ret;
+        }
+
+        /// <summary>
+        /// Ends the process of writing out the PDF and closes out the file
+        /// </summary>
+        /// <returns></returns>
+        public bool EndDocumentHandler()
+        {
+            int value = Interop.TessPdfRenderer.Native.TessPDFRendererEndDocumentHandler(handle);
+            bool ret = Convert.ToBoolean(value);
+
+            return ret;
+        }
+
+        /// <summary>
+        /// Gets or sets default <see cref="PageSegMode" /> mode used by <see cref="Tesseract.TesseractEngine.Process(Pix, Rect, PageSegMode?)" />.
+        /// </summary>
+        public PageSegMode DefaultPageSegMode
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// The reference to the native DLL class instance
+        /// </summary>
+        internal HandleRef Handle
+        {
+            get { return handle; }
+        }
+
+    }
+}
+
